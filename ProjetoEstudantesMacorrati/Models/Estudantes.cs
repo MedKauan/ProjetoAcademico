2ca3dcaac969433640a1748ad3b9eeca063c8b0b
kutt.kauan <kauan_med@outlook.com>
2021-06-25 22:24:41 -0300

    Add project files.

---
+++ ProjetoEstudantesMacorrati/Models/Estudantes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UniversidadeMacoratti.Models;
+
+namespace ProjetoEstudantesMacorrati.Models
+{
+    public class Estudantes
+    {
+        public int EstudanteID { get; set; }
+        public string SobreNome { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataMatricula { get; set; }
+        // A listagem de matriculas que o estudante possui.
+        public ICollection<Matricula> Matriculas { get; set; }
+    }
+}
---
+++ ProjetoEstudantesMacorrati/Models/Matricula.cs
@@ -0,0 +1,24 @@
+﻿using ProjetoEstudantesMacorrati.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversidadeMacoratti.Models
+{
+    public enum Nota
+    {
+        A, B, C, D, F
+    }
+
+    public class Matricula
+    {
+        public int MatriculaID { get; set; }
+        public int CursoID { get; set; }
+        public int EstudanteID { get; set; }
+        public Nota? Nota { get; set; }
+        public Curso Curso { get; set; }
+        public Estudantes Estudante { get; set; }
+
+    }
+}
